494703af0b518e1b01dc98272d05e8f66a9ecf45
Igor Solfa Romero <igorsolfaromero@gmail.com>
2022-07-15 21:49:55 -0300

    feat: add index test

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,19 @@
+import App from "../app"
+import IndexRoute from "../routes/index.route"
+
+import request from 'supertest'
+
+afterAll(async () => {
+  await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
+})
+
+describe(`Testing Index`, () => {
+  describe(`[GET] /`, () => {
+    it(`response statusCode 200`, () => {
+      const indexRoute = new IndexRoute()
+      const app = new App([indexRoute])
+
+      return request(app.getServer()).get(`${indexRoute.path}`).expect(200)
+    })
+  })
+})
